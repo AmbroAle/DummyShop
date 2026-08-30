8d79e45cd79318a93cc43adbf85582803d162369
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2026-08-30 14:55:24 +0200

    added functions and logic for cartcontext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -9,9 +9,9 @@ interface CartItem {
 interface CartContextType {
   cartItems: CartItem[];
   addToCart: (product: Product) => void;
-  removeFromCart: (productId: number) => void;
-  increaseQuantity: (productId: number) => void;
-  decreaseQuantity: (productId: number) => void;
+  removeFromCart: (productId: Product) => void;
+  increaseQuantity: (productId: Product) => void;
+  decreaseQuantity: (productId: Product) => void;
   clearCart: () => void;
   totalItems: number;
   totalPrice: number;
@@ -34,4 +34,55 @@ export function CartProvider({children}:{children: ReactNode}){
             return [...prev, {product, quantity: 1}]
         });
     };
+
+    const removeFromCart = (product : Product) => {
+        setCartItems(prev => prev.filter(item => item.product.id != product.id));
+    }
+
+    const increaseQuantity = (product : Product) => {
+        setCartItems(prev => prev.map(item => item.product.id == product.id ? {...item, quantity: item.quantity + 1} : item))
+    }
+
+    const decreaseQuantity = (product : Product) => {
+        setCartItems(prev => prev.map(item => item.product.id == product.id ? {...item, quantity: item.quantity - 1} : item).filter(item => item.quantity > 0))
+    }
+
+    const clearCart = () => {
+        setCartItems([]);
+    };
+
+    const totalItems = cartItems.reduce((total, item) => total + item.quantity,0);
+
+    const totalPrice = cartItems.reduce(
+        (total, item) =>
+        total + item.product.price * item.quantity,
+        0
+    );
+
+    return (
+        <CartContext.Provider
+        value={{
+            cartItems,
+            addToCart,
+            removeFromCart,
+            increaseQuantity,
+            decreaseQuantity,
+            clearCart,
+            totalItems,
+            totalPrice
+        }}
+        >
+        {children}
+        </CartContext.Provider>
+    );
 };
+
+export function useCart() {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error('useCart must be used inside CartProvider');
+  }
+
+  return context;
+}
