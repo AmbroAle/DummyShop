75c77a5c5f3efaa9a9ce140666d2d18c75b3dc9e
Alessandro Ambrogiani <alessandr.ambrogian4@studio.unibo.it>
2026-08-31 18:00:35 +0200

    added badge notification in Cart button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import ProductCard from '../components/ProductCard';
 import { getProducts, getCategories, getProductsByCategory, searchProducts, Product } from '../services/api';
 import ProductModal from '../components/ProductModal';
 import CartAside from '../components/CartAside.tsx';
+import Badge from '@mui/material/Badge';
+import { useCart } from '../context/CartContext';
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -18,6 +20,7 @@ export default function Home() {
   const [totalProducts, setTotalProducts] = useState<number>(0);
   const limit = 20;
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const { totalItems } = useCart();
 
   useEffect(() => {
     getCategories().then(data => setCategories(data));
@@ -81,24 +84,26 @@ export default function Home() {
           onChange={(e) => setSearchQuery(e.target.value)}
           style={{ width: '1000px', padding: '10px', background: '#1a1a1a', border: '1px solid #333', color: 'white' }}
         />
-        <button className="btn-add" onClick={() => setIsCartOpen(true)} style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            width="18" 
-            height="18" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            stroke="currentColor" 
-            strokeWidth="2" 
-            strokeLinecap="round" 
-            strokeLinejoin="round"
-          >
-            <circle cx="9" cy="21" r="1"></circle>
-            <circle cx="20" cy="21" r="1"></circle>
-            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-          </svg>
-          CART
-        </button>
+        <Badge badgeContent= {totalItems} sx={{'& .MuiBadge-badge':{backgroundColor: '#ff3d00', color: 'white'}}}>
+          <button className="btn-add" onClick={() => setIsCartOpen(true)} style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="18" 
+              height="18" 
+              viewBox="0 0 24 24" 
+              fill="none" 
+              stroke="currentColor" 
+              strokeWidth="2" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            >
+              <circle cx="9" cy="21" r="1"></circle>
+              <circle cx="20" cy="21" r="1"></circle>
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg>
+            CART
+          </button>
+        </Badge>
       </header>
 
       <div className="main-content">
